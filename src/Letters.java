72e95a61f097223d9e4ff29101b4dea7b9783d8f
sanchale000 <sanchale000@CAM-LAB-24.bgsd.k12.wa.us>
2019-09-16 07:54:23 -0700

    Finished? Dad

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,25 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Dad();
+		
 
 	}
-
+	public static void Dad() {
+		Para1();
+		Para2D();		
+	}
+	public static void Para1() {
+		System.out.println("Hey!  How are you?  I'm doing quite fine for myself.\nFor the most part, events in my life are proceeding swimmingly\nMy cat's dead, which is a little dissappointing, but other than that I'm mostly fine.\nSchool is going well, even though it's barely been a couple weeks since it started.");
+		
+	}
+	public static void Para2D() {
+		System.out.println("Anyways, dad, what I'd like to ask about is your job.\nYou see, I need to know some information for my college applications so knowing a couple things would be nice.\nIf you could tell me when you gained employment at your current position, the title of your current position, and your employer, that would be great.\nI'll be hoping to hear from you soon.\n\t-Alex S.\n");
+	}
+	public static void Mom() {
+		Para1();
+		Para2M();
+	}
+	public static void Para2m();
+		System.out.println("Now, the main focus of")
 }
