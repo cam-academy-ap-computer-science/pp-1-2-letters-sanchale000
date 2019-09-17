1211cd535f1aff88240c13898a39808f643799d0
sanchale000 <sanchale000@CAM-LAB-24.bgsd.k12.wa.us>
2019-09-17 07:19:29 -0700

    Done(?)

--- src/Letters.java
+++ src/Letters.java
@@ -3,7 +3,8 @@ public class Letters {
 
 	public static void main(String[] args) {
 		Dad();
-		
+		Mom();
+		Bro();
 
 	}
 	public static void Dad() {
@@ -11,7 +12,7 @@ public class Letters {
 		Para2D();		
 	}
 	public static void Para1() {
-		System.out.println("Hey!  How are you?  I'm doing quite fine for myself.\nFor the most part, events in my life are proceeding swimmingly\nMy cat's dead, which is a little dissappointing, but other than that I'm mostly fine.\nSchool is going well, even though it's barely been a couple weeks since it started.");
+		System.out.println("Hey!  How are you?  I'm doing quite fine for myself.\nFor the most part, events in my life are proceeding swimmingly.\nMy cat's dead, which is a little dissappointing, but other than that I'm mostly fine.\nSchool is going well, even though it's barely been a couple weeks since it started.");
 		
 	}
 	public static void Para2D() {
@@ -21,6 +22,14 @@ public class Letters {
 		Para1();
 		Para2M();
 	}
-	public static void Para2m();
-		System.out.println("Now, the main focus of")
+	public static void Para2M() {
+		System.out.println("Now, the main purpose of this is actually to ask about taxes.\nIt happens that I need your tax information for my college applications.\nTelling me your and Dad's gross income over the last two years, as well as tax returns, would be helpful.\nIf you could get back to me in a week or two I would appreciate it.\n\t-Alex S.\n");
+	}
+	public static void Bro() {
+		Para1();
+		Para2B();
+	}
+	public static void Para2B() {
+		System.out.println("To finish off here, what I actually need is a small amount of cash.\nCollege is expensive, so I can use Just about whatever money I can get, including just a little bit of yours.\nI'm out about $3000, due to \"various expenses.\"\nIf you'll remember, you still owe me from a year-or-two ago, so hopefully you'll be appropriately generous.\n\t-Alex S.\n");
+	}
 }
